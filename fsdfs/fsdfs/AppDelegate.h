4f1999853e8d6aef963529e3b4bb7796ad56e0e3
余敏侠 <yuminxia@yuminxiademac-mini.local>
2016-03-19 09:29:05 +0800

    先测试

---
+++ fsdfs/fsdfs/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fsdfs
+//
+//  Created by 余敏侠 on 16/3/19.
+//  Copyright © 2016年 secrui Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ fsdfs/fsdfs/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  fsdfs
+//
+//  Created by 余敏侠 on 16/3/19.
+//  Copyright © 2016年 secrui Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
